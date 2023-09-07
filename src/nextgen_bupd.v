5f9d3d7bdf19a62c1c13b84888969f83c2acb1aa
ageorg29 <ageorges@cs.au.dk>
2023-09-07 16:51:14 +0200

    version that is frame preserving v2

--- src/nextgen_bupd.v
+++ src/nextgen_bupd.v
@@ -44,27 +44,31 @@ Class GenTransExtra {A : cmra} (f : A -> A) := {
 Local Program Definition uPred_restr_bupd_def {M : ucmra}
   (t : M → M) `{!CmraMorphism t} `{!GenTransExtra t} (Q : uPred M) : uPred M :=
   {| uPred_holds n x := ∀ k yf,
-      k ≤ n -> ✓{k} (x ⋅ yf) -> ∃ x' x1' x2', x' ≡{k}≡ x1' ⋅ x2' /\ x1' ≡{k}≡ t x1' /\ (∃ x2, x ≡{k}≡ x2 ⋅ x2')
-                                            /\ (∀ x2'' yf', x2' ≡{k}≡ t x2'' -> yf ≡{k}≡ t yf' -> ✓{k} (x1' ⋅ x2'' ⋅ yf'))
+      k ≤ n -> ✓{k} (x ⋅ yf) -> ∃ x' x1' x2', x' ≡{k}≡ x1' ⋅ x2' /\ x1' ≡{k}≡ t x1'
+                                            /\ (∃ x2, x ≡{k}≡ x2 ⋅ x2' /\ (∀ x3 x2'' yf', x2 ≡{k}≡ t x3 -> x2' ≡{k}≡ t x2'' -> yf ≡{k}≡ t yf' ->
+                                                                                        ✓{k} (x3 ⋅ x2'' ⋅ yf') -> ✓{k} (x1' ⋅ x2'' ⋅ yf')))
                                             /\ ✓{k} (x' ⋅ yf)
                                             /\ Q k x' |}.
 Next Obligation.
   intros M t Htrans Htransextra Q n1 n2 x1 x2 HQ [x3 Hx] Hn k yf Hk (* [x1' [x2' [Heq1 Ht]]] *).
   rewrite {1}(dist_le _ _ _ _ Hx); last lia. intros Hxy.
   simpl in *.
-  destruct (HQ k (x3 ⋅ yf)) as (x'&x1'&x2'&Heq'&Heq2&Hx'&Himpl&Hv&HQ'); [lia|auto|..].
+  destruct (HQ k (x3 ⋅ yf)) as (x'&x1'&x2'&Heq'&Heq2&(x2''&Hx2'&Himpl)&Hv&HQ'); [lia|auto|..].
   { by rewrite assoc. (* rewrite -assoc (comm _ x3 yf) assoc in Hxy. apply cmra_validN_op_l in Hxy; auto.  *)}
   exists (x' ⋅ x3),x1',(x2' ⋅ x3). rewrite (assoc op) -Heq'.
   split;[|split];auto. rewrite assoc in Hv. split;[|split];auto.
-  - destruct Hx' as [x2'' Hx2']. exists (x2'').
+  - exists (x2'').
     rewrite (dist_le _ _ _ _ Hx); last lia. rewrite assoc Hx2' //.
-  - intros x2'' yf' Heqx2'' Heqyf.
+    split;auto.
+    intros x0 x2''0 yf' HH Heqx2'' Heqyf.
     symmetry in Heqx2''.
     apply gen_trans_op_inv in Heqx2'' as Ha.
     destruct Ha as [a1 [a2 [Ha12 [Ha1 Ha2]]]].
-    symmetry in Ha1. apply (Himpl _ (a2 ⋅ yf')) in Ha1 as Hii;[|rewrite cmra_morphism_op Ha2 -Heqyf//].
-    rewrite Ha12. rewrite assoc in Hii. rewrite assoc. auto.
-  - split;auto. eapply uPred_mono;eauto. by exists x3.
+    symmetry in Ha1. intros Hx0.
+    apply (Himpl _ _ (a2 ⋅ yf') HH) in Ha1 as Hii;[|rewrite cmra_morphism_op Ha2 -Heqyf//|].
+    + rewrite Ha12. rewrite assoc in Hii. rewrite assoc. auto.
+    + rewrite assoc. rewrite -(assoc _ _ a1 a2). rewrite -Ha12. auto.
+  - eapply uPred_mono;eauto. by exists x3.
 Qed.
 
 
@@ -105,23 +109,27 @@ Section restr_bupd_rules.
     apply (cmra_morphism_validN f) in Hv.
     rewrite cmra_morphism_op in Hv.
     apply Hcond in Hv as Hx';auto.
-    destruct Hx' as (x'&x1'&x2'&Heq1&Heq2&Hfeq&Himpl&Hv'&HP).
-    destruct Hfeq as [x2 Hfeq].
+    destruct Hx' as (x'&x1'&x2'&Heq1&Heq2&(x2&Hfeq&Himpl)&Hv'&HP).
     apply gen_trans_op_inv in Hfeq as Ha.
     destruct Ha as [a1 [a2 [Ha12 [Ha1 Ha2]]]].
     exists (x1' ⋅ a2). eexists. eexists.
+    symmetry in Ha1. symmetry in Ha2.
+    eapply Himpl in Ha1 as HH;eauto;cycle 1.
+    { rewrite -Ha12. auto. }
+    
     repeat split;eauto.
-    { intros. rewrite H in Ha2. rewrite -Hidemp in Ha2. symmetry in Ha2. eapply Himpl in Ha2;[eauto|].
-      rewrite H0. rewrite -Hidemp. auto.
+    { exists a1. split;auto. intros. rewrite H0 in Ha2. rewrite -Hidemp in Ha2.
+      rewrite H in Ha1. rewrite -Hidemp in Ha1. eapply Himpl in Ha2;eauto.
+      rewrite H1. rewrite -Hidemp. auto.
     }
-    { rewrite cmra_morphism_op Ha2 -Heq2 -Heq1. auto. }
+    { rewrite cmra_morphism_op -Ha2 -Heq2 -Heq1. auto. }
   Qed.
 
   Lemma restr_bupd_ne : NonExpansive (@uPred_restr_bupd M f _ _).
   Proof.
     intros n P Q HPQ.
     unseal. split. intros. simpl in *.
-    split =>HH k ? Hle B;pose proof (HH _ _ Hle B) as (?&?&?&?&?&(?&?)&?&?&?); do 3 eexists;(split;[apply H1|..]);repeat (split;eauto).
+    split =>HH k ? Hle B;pose proof (HH _ _ Hle B) as (?&?&?&?&?&(?&?&?)&?&?); do 3 eexists;(split;[apply H1|..]);repeat (split;eauto).
     all: (apply HPQ;auto;[lia|..]; eauto using cmra_validN_op_l).
   Qed.
 
@@ -129,7 +137,7 @@ Section restr_bupd_rules.
   Proof.
     unseal. split;intros ??? Hcond.
     simpl in *. intros k yf Hle Hv.
-    apply Hcond in Hv as (?&?&?&?&?&(?&?)&?&?&?);eauto.
+    apply Hcond in Hv as (?&?&?&?&?&(?&?&?)&?&?);eauto.
   Qed.
 
   Lemma restr_bupd_frame_l P R : (|=/f=> P) ∗ R ⊢ |=/f=> P ∗ R.
@@ -137,19 +145,23 @@ Section restr_bupd_rules.
     unseal. split.
     intros n x Hx [x1 [x2 [Heq [Hop HR]]]].
     intros k yf Hk. rewrite {1}(dist_le _ _ _ _ Heq); last lia. intros Hv. simpl in *.
-    edestruct (Hop k (x2 ⋅ yf)) as [x' [x1' [x2' [Heq1 [Heq2 [[x3 Hx3] [Hcond2 [Hv' HP']]]]]]]];auto.
+    edestruct (Hop k (x2 ⋅ yf)) as [x' [x1' [x2' [Heq1 [Heq2 [[x3 [Hx3 Hcond2]] [Hv' HP']]]]]]];auto.
     { by rewrite assoc. (* rewrite -assoc (comm _ x2 yf) assoc in Hv. by apply cmra_validN_op_l in Hv.  *)}
     (* rewrite assoc in Hx'. *)
     exists (x' ⋅ x2),(x1'),(x2' ⋅ x2).
     repeat split;eauto.
     - rewrite Heq1 assoc. auto.
     - exists (x3). rewrite assoc -Hx3. rewrite (dist_le _ _ _ _ Heq);auto.
-    - intros x2'' yf' Heqf Hyf'.
+      split;auto.
+      intros x0 x2'' yf' Heq0 Heqf Hyf'.
       symmetry in Heqf.
       apply gen_trans_op_inv in Heqf as Ha.
       destruct Ha as [a1 [a2 [Ha12 [Ha1 Ha2]]]].
-      symmetry in Ha1. eapply Hcond2 in Ha1 as Ha1';cycle 1.
+      symmetry in Ha1. intros Hvv.
+      eapply Hcond2 in Ha1 as Ha1';cycle 1.
+      { apply Heq0. }
       { instantiate (1:=a2 ⋅ yf'). rewrite cmra_morphism_op -Ha2 -Hyf'. auto. }
+      { rewrite assoc -(assoc _ _ a1 a2) -Ha12//. }
       rewrite Ha12. rewrite assoc in Ha1'. rewrite assoc. auto.
     - by rewrite -assoc.
     - exists x', x2. repeat split; auto.
